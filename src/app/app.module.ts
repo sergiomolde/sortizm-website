27f99435b271bb4a4a3ed564d3044988d63b375a
Sergio Ortiz Molde <sergioortizmolde@gmail.com>
2022-09-07 14:37:59 +0200

    Added a toolbar for the navigation and thinking about the desing of main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
+import {MatCardModule} from '@angular/material/card';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,12 +15,14 @@ import { AppComponent } from './app.component';
 import { IndexComponent } from './components/index/index.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IndexComponent,
-    SidenavComponent
+    SidenavComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +30,11 @@ import { SidenavComponent } from './components/sidenav/sidenav.component';
     BrowserAnimationsModule,
     MatSidenavModule,
     MatIconModule,
-    MatDividerModule
+    MatDividerModule,
+    MatCardModule,
+    MatProgressBarModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
