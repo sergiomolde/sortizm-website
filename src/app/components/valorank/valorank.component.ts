c86e4f56c0c91149fb00c8976a74c274e96828a6
Sergio Ortiz Molde <sergioortizmolde@gmail.com>
2022-09-15 01:07:30 +0200

    Added some styling to index, valorank api site and some styles

--- src/app/components/valorank/valorank.component.ts
+++ src/app/components/valorank/valorank.component.ts
@@ -15,11 +15,16 @@ export class ValorankComponent implements OnInit {
   example = true;
   exampledata = JSON.stringify(exampledata, undefined, 2);
   trueData: any = undefined;
+  botsCommand = [
+    {name: "Nightbot", value: ''},
+  ]
+  botSelected? = '';
   form = this.fb.group({
     language: ['', Validators.required],
     region: ['', [Validators.required]],
     riotname: ['', Validators.required],
-    riotid: ['', Validators.required]
+    riotid: ['', Validators.required],
+    botname: ['']
   });
 
   ngOnInit(): void {
@@ -32,9 +37,11 @@ export class ValorankComponent implements OnInit {
       riotname: `/${this.form.get('riotname')?.value}`,
       riotid: `/${this.form.get('riotid')?.value}`
     }
-    this.requestmade = `http://www.sortizm-dev.es/valorank${this.form.get('language')?.value}${this.form.get('region')?.value}/${this.form.get('riotname')?.value}/${this.form.get('riotid')?.value}`
+    this.requestmade = `http://sortizm-dev.es/valorank${this.form.get('language')?.value}${this.form.get('region')?.value}/${this.form.get('riotname')?.value}/${this.form.get('riotid')?.value}`
+    let botCommand = this.botsCommand.filter((result) => this.form.get('botname')?.value === result.name);
+    botCommand[0].value = '!addcom !valorank $(eval re = `$(urlfetch json ' + this.requestmade + ')`; try { json = JSON.parse(re); `${json.data}`} catch(e) {' + "'!valorank under maintenance'" + "})";   
+    this.botSelected = `${this.form.get('botname')?.value}: ${botCommand[0].value}`;
     this.apiService.getValorankData(formData).subscribe((resp) => {
-    console.log(this.trueData);
     this.trueData = JSON.stringify(resp, undefined, 2);
     })
 
